50d7bef01b0ff1a56efc634c14ea786c20d7a8ad
Shridhar PA <shridharshri2005@gmail.com>
2026-03-23 09:53:12 +0530

    Time: 2 ms (59.21%), Space: 43.8 MB (61.94%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n=code.length;
+        int[] res=new int[n];
+        for(int i=0;i<n;i++){
+            int sum=0;
+            if(k>0){
+                for(int j=1;j<=k;j++){
+                    sum+=code[(i+j)%n];
+                }
+            }
+                else if(k<0){
+                    for(int j=1;j<=-k;j++) {
+                        sum+=code[(i-j+n)%n];
+                    }
+                }
+            res[i]=sum;
+        }
+        return res;
+    }
+}
