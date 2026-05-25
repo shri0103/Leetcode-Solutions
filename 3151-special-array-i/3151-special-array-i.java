e2ffc6d9c90016b7a5b22d3110cb861048d4037d
Shridhar PA <shridharshri2005@gmail.com>
2026-05-25 09:47:33 +0530

    Time: 0 ms (100%), Space: 44.8 MB (84.28%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.java
+++ 3151-special-array-i/3151-special-array-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        int[] arr=new int[2];
+
     for(int i=1;i<nums.length;++i){
         if(nums[i-1]%2==nums[i]%2){
             return false;
