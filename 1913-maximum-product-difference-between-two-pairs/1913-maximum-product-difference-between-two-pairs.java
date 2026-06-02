d52644c27d2ded1195b1c399561aa57f141fbd03
Shridhar PA <shridharshri2005@gmail.com>
2026-06-02 21:31:45 +0530

    Time: 10 ms (62.96%), Space: 48 MB (11.4%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
@@ -4,7 +4,7 @@ class Solution {
         int a=nums.length;
         int n=nums[0]*nums[1];
         int s=nums[a-1]*nums[a-2];
-        return Math.abs(n-s);
+        return s-n;
 
         
     }
