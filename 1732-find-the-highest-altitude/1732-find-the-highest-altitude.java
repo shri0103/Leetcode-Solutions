44b75e6042aeeda7e35aa1a6d5e740346ba61d2f
Shridhar PA <shridharshri2005@gmail.com>
2026-06-19 09:30:43 +0530

    Time: 0 ms (100%), Space: 43.3 MB (6.14%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,14 +1,14 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        int n = gain.length;
-        int max = 0;
-        int temp = 0;
-        for (int i = 0; i < n; i++) {
-            temp = temp + gain[i];
-            if (max < temp) {
-                max = temp;
-            }
-        }
-        return max;
+      int altitude =0;
+      int max=0;
+      for(int num:gain){
+        altitude+=num;
+        max=Math.max(max,altitude);
+      }
+      return max;
+
+
+        
     }
 }
