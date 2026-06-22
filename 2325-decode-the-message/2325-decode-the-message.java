a481f58df53228133c56d7979d18a0a1500e0813
Shridhar PA <shridharshri2005@gmail.com>
2026-06-22 10:18:15 +0530

    Time: 2 ms (100%), Space: 43.5 MB (89.89%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -1,21 +1,24 @@
 class Solution {
     public String decodeMessage(String key, String message) {
+        char[] map = new char[128];
+        char curr = 'a';
+
+        for (char c : key.toCharArray()) {
+            if (c != ' ' && map[c] == 0) {
+                map[c] = curr++;
+            }
+        }
+
         StringBuilder ans = new StringBuilder();
-        key = key.replaceAll(" ", "");
-        HashMap<Character,Character> letters = new HashMap<>();
-        char original = 'a';
-        for (int i = 0; i < key.length() ; i++) {
-            if (!letters.containsKey(key.charAt(i))){
-                letters.put(key.charAt(i),original++);
+
+        for (char c : message.toCharArray()) {
+            if (c == ' ') {
+                ans.append(' ');
+            } else {
+                ans.append(map[c]);
             }
         }
-        for (int i = 0; i < message.length(); i++) {
-            if (letters.containsKey(message.charAt(i))){
-                ans.append(letters.get(message.charAt(i)));
-            }else{
-                ans.append(message.charAt(i));
-            }
-        }
+
         return ans.toString();
     }
 }
