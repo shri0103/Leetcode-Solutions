0e93ca1f18937c68c1f900ff9b4d72d204f28718
Shridhar PA <shridharshri2005@gmail.com>
2026-07-01 14:20:26 +0530

    Time: 2 ms (100%), Space: 97.4 MB (99.21%) - LeetHub

--- 3891-minimum-increase-to-maximize-special-indices/3891-minimum-increase-to-maximize-special-indices.java
+++ 3891-minimum-increase-to-maximize-special-indices/3891-minimum-increase-to-maximize-special-indices.java
@@ -1,44 +1,19 @@
 class Solution {
-    long r(int[] nums, int pos, int skip, long[][] dp) {
-        int n = nums.length;
-        if (pos >= n - 1) return 0;
-
-        if (dp[skip][pos] != -1) return dp[skip][pos];
-
-        long cost = Math.max(0, Math.max(nums[pos - 1], nums[pos + 1]) + 1L - nums[pos]);
-
-        if (skip == 1) {
-            return dp[skip][pos] = cost + r(nums, pos + 2, skip, dp);
-        }
-
-        long takeNormal = cost + r(nums, pos + 2, 0, dp);
-        long takeSkip   = cost + r(nums, pos + 3, 1, dp);
-
-        dp[1][pos] = takeSkip;
-        dp[0][pos] = takeNormal;
-
-        return dp[skip][pos] = Math.min(takeNormal, takeSkip);
-    }
-
     public long minIncrease(int[] nums) {
         int n = nums.length;
-
-        // Odd length: fixed pattern
-        if (n % 2 == 1) {
-            long ans = 0;
+        long min = 0;
+        if ((n & 1) == 1) {
+            for (int i = 1; i < n; i += 2) min += Math.max(Math.max(nums[i - 1], nums[i + 1]) + 1 - nums[i], 0);
+        } else {
+            long[] starting = new long[]{0, 0};
             for (int i = 1; i < n - 1; i += 2) {
-                ans += Math.max(0, Math.max(nums[i - 1], nums[i + 1]) + 1L - nums[i]);
+                int firstOp = Math.max(Math.max(nums[i - 1], nums[i + 1]) + 1 - nums[i], 0);
+                int secondOp = Math.max(Math.max(nums[i], nums[i + 2]) + 1 - nums[i + 1], 0);
+                starting[1] = Math.min(starting[0], starting[1]) + secondOp;
+                starting[0] += firstOp;
             }
-            return ans;
+            min = Math.min(starting[0], starting[1]);
         }
-
-        // Even length: DP
-        long[][] dp = new long[2][n];
-        for (long[] row : dp) Arrays.fill(row, -1);
-
-        return Math.min(
-            r(nums, 1, 0, dp),
-            r(nums, 2, 1, dp)
-        );
+        return min;
     }
 }
